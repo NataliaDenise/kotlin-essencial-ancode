e73508c2c36d6ed54dc5d5e7e93cd86f01ef6627
NataliaDenise <Caceres.nataliaad@gmail.com>
2023-01-27 14:55:16 -0300

    VentaCelu

--- src/Constructor/Main.kt
+++ src/Constructor/Main.kt
@@ -1,7 +1,7 @@
 package Constructor
 
 fun main() {
-    var miiPhones = iPhones(50,40,1550.50)
+    val miiPhones = iPhones(50,40,1550.50)
     val celulares = miiPhones.ganancia()
-    print("La ganancia fue de $celulares")
+    println("La ganancia fue de UDS$celulares")
 }
